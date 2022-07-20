32982705fb876e518c2e06e1d65e6e1fe45ae139
marceldietl <ge93coc>
2022-07-20 14:13:00 +0200

    Added route description

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 function changeBg() {
-    var navbar = document.getElementById('navbar')
+    var navbar = document.getElementById('navbar');
     var scrollValue = window.scrollY;
     if(scrollValue < 500) {
         navbar.classList.remove('navbarColor');
     } else {
-        navbar.classList.add('navbarColor')
+        navbar.classList.add('navbarColor');
     }
 }
+window.addEventListener('scroll', changeBg);
 
-window.addEventListener('scroll', changeBg)
+window.addEventListener("hashchange", function() { scrollBy(0, -50) })
+
+
